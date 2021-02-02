0852b3f759047b695ef0edb6c01ae9f9fd0086a1
Xin Liu <liu.xin7@northeastern.edu>
2021-02-01 20:56:27 -0800

    Added spring generated files

---
+++ src/test/java/com/example/wbdvsp2101xinliuserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101xinliuserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
